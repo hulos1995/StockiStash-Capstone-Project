a5fc34397e403ff7238c58ce9f00c86eb685fec6
Bao <bao9600455@yahoo.com.vn>
2024-06-25 15:19:13 -0400

    style header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,47 @@
 import { Link } from "react-router-dom";
 import "./Header.scss";
-const Header = ({ isDarkMode }) => {
+
+const Header = ({ isDarkMode, toggleDarkMode }) => {
   return (
     <header
       className={`header ${isDarkMode ? "header--dark" : "header--light"}`}
     >
-      <nav className="header__nav">
-        <ul className="header__nav-list">
-          <li className="header__nav-item">
-            <Link to="/" className="header__nav-link">
-              Home
-            </Link>
-          </li>
-          <li className="header__nav-item">
-            <Link to="/inventory" className="header__nav-link">
-              Inventory
-            </Link>
-          </li>
-          <li className="header__nav-item">
-            <Link to="/signup" className="header__nav-link">
-              Register
-            </Link>
-          </li>
-          <li className="header__nav-item">
-            <Link to="/login" className="header__nav-link">
-              Login
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="header__container">
+        <button
+          className={`header__dark-mode-toggle ${
+            isDarkMode
+              ? "header__dark-mode-toggle--dark"
+              : "header__dark-mode-toggle--light"
+          }`}
+          onClick={toggleDarkMode}
+        >
+          {isDarkMode ? "🌞" : "🌜"}
+        </button>
+        <nav className="header__nav">
+          <ul className="header__nav-list">
+            <li className="header__nav-item">
+              <Link to="/" className="header__nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="header__nav-item">
+              <Link to="/inventory" className="header__nav-link">
+                Inventory
+              </Link>
+            </li>
+            <li className="header__nav-item">
+              <Link to="/signup" className="header__nav-link">
+                Register
+              </Link>
+            </li>
+            <li className="header__nav-item">
+              <Link to="/login" className="header__nav-link">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
