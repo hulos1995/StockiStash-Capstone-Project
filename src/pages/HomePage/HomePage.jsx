820ed2dbcf52fee926b237ec6186abea960c856a
Bao <bao9600455@yahoo.com.vn>
2024-06-27 14:34:20 -0400

    add page body and slider

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,11 @@
 import Hero from "../../components/Hero/Hero";
-
+import PageBody from "../../components/PageBody/PageBody";
 const HomePage = () => {
   return (
     <main>
       <Hero />
+      <PageBody />
     </main>
   );
 };
-
 export default HomePage;
