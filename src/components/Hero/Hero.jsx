ea2e737dd49e4396d043915cf5c744a479d27813
Bao <bao9600455@yahoo.com.vn>
2024-06-27 11:24:15 -0400

    responsive hero for homepage

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import "./Hero.scss";
+const Hero = () => {
+  return (
+    <section>
+      <div className="hero">
+        <div className="hero__container">
+          <h1 className="hero__container-title">Stockistash</h1>
+          <p className="hero__container-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+            tempore nisi repudiandae debitis quas quo possimus molestiae,
+            necessitatibus quia odio cum perspiciatis obcaecati soluta
+            praesentium eaque aut dicta, odit saepe.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Hero;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,11 @@
+import Hero from "../../components/Hero/Hero";
+
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
 };
 
 export default HomePage;
