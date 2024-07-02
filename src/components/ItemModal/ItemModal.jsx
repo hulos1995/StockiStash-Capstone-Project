3be7890340c264521757c373594a4099708d9556
Bao <bao9600455@yahoo.com.vn>
2024-07-02 09:43:19 -0400

    create basic modal and set condition to display grits to item that has grids

---
+++ src/components/ItemModal/ItemModal.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import "./ItemModal.scss";
+
+const ItemModal = ({ show, handleClose, children, selectedItem }) => {
+  const showHideClassName = show ? "modal display-block" : "modal display-none";
+  const [selectedGrit, setSelectedGrit] = useState(null);
+
+  useEffect(() => {
+    if (selectedItem && selectedItem.grits && selectedItem.grits.length > 0) {
+      setSelectedGrit(selectedItem.grits[0]);
+    }
+  }, [selectedItem]);
+
+  const handleGritClick = (grit) => {
+    setSelectedGrit(grit);
+  };
+
+  return (
+    <div className={showHideClassName}>
+      {selectedItem && selectedGrit && (
+        <section className="modal-main">
+          {children}
+          <div className="modal-image">
+            <img
+              src={selectedGrit.image}
+              alt={`${selectedGrit.grit} grit picture`}
+            />
+          </div>
+          <div className="modal-details">
+            <div className="modal-details__info">
+              <p>Status: {selectedItem.status}</p>
+              <p>Description: {selectedItem.description}</p>
+              <p>Quantity: {selectedItem.quantity}</p>
+            </div>
+            <div className="modal-grits">
+              {selectedItem.grits.map((grit) => (
+                <div
+                  key={grit.id}
+                  className={`modal-grit ${
+                    selectedGrit.id === grit.id ? "active" : ""
+                  }`}
+                  onClick={() => handleGritClick(grit)}
+                >
+                  <p>{grit.grit}</p>
+                </div>
+              ))}
+            </div>
+            <div className="modal-grit-details">
+              <p>Grit: {selectedGrit.grit}</p>
+              <p>Description: {selectedGrit.description}</p>
+              <p>Quantity: {selectedGrit.quantity}</p>
+            </div>
+          </div>
+          <button onClick={handleClose}>Close</button>
+        </section>
+      )}
+    </div>
+  );
+};
+
+export default ItemModal;
--- src/pages/InventoryPage/InventoryPage.jsx
+++ src/pages/InventoryPage/InventoryPage.jsx
@@ -1,10 +1,16 @@
 import { useState, useEffect } from "react";
 import "./InventoryPage.scss";
 import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+import ItemModal from "../../components/ItemModal/ItemModal";
+
 const InventoryPage = ({ isDarkMode }) => {
+  const { id } = useParams();
   const [inventoryData, setInventoryData] = useState([]);
   const [selectedItem, setSelectedItem] = useState(null);
+  const [show, setShow] = useState(false);
   const base_URL = import.meta.env.VITE_API_URL;
+
   useEffect(() => {
     const getInventory = async () => {
       try {
@@ -14,11 +20,31 @@ const InventoryPage = ({ isDarkMode }) => {
         console.error("Error fetching data", error);
       }
     };
+
+    const getInventoryDetails = async () => {
+      try {
+        const res = await axios.get(`${base_URL}/inventory/${id}`);
+        setSelectedItem(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (id) {
+      getInventoryDetails();
+    }
     getInventory();
-  }, []);
-  const handleItemClick = (item) => {
+  }, [base_URL, id]);
+
+  const showModal = (item) => {
     setSelectedItem(item);
+    setShow(true);
   };
+
+  const hideModal = () => {
+    setShow(false);
+  };
+
   return (
     <div
       className={`inventory ${
@@ -34,39 +60,46 @@ const InventoryPage = ({ isDarkMode }) => {
         <button className="inventory__button">Search</button>
       </div>
       <div className="inventory__items">
+        <ItemModal
+          selectedItem={selectedItem}
+          show={show}
+          handleClose={hideModal}
+        />
         {inventoryData.map((item) => (
-          <div
-            key={item.id}
-            className="inventory__item"
-            onClick={() => handleItemClick(item)}
-          >
-            <img
-              src={item.image}
-              alt={item.name}
-              className="inventory__item-image"
-            />
-            <div className="inventory__item-details">
-              <h3 className="inventory__item-name">{item.item_name}</h3>
-              <p className="inventory__item-description">{item.description}</p>
-              <p className="inventory__item-description">
-                Quantity: {item.quantity}
-              </p>
-              <p className="inventory__item-description">
-                Status:{" "}
-                <span
-                  className={`inventory__item-status ${
-                    item.status === "Out of Stock"
-                      ? "inventory__item-status--outstock"
-                      : "inventory__item-status--instock"
-                  }`}
-                >
-                  {item.status}
-                </span>
-              </p>
-              <a href={item.link} className="inventory__item-link">
-                Click here to buy
-              </a>
-            </div>
+          <div key={item.id} className="inventory__item-wrapper">
+            <Link to={`/inventory/${item.id}`} onClick={() => showModal(item)}>
+              <div className="inventory__item">
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className="inventory__item-image"
+                />
+                <div className="inventory__item-details">
+                  <h3 className="inventory__item-name">{item.item_name}</h3>
+                  <p className="inventory__item-description">
+                    {item.description}
+                  </p>
+                  <p className="inventory__item-description">
+                    Quantity: {item.quantity}
+                  </p>
+                  <p className="inventory__item-description">
+                    Status:{" "}
+                    <span
+                      className={`inventory__item-status ${
+                        item.status === "Out of Stock"
+                          ? "inventory__item-status--outstock"
+                          : "inventory__item-status--instock"
+                      }`}
+                    >
+                      {item.status}
+                    </span>
+                  </p>
+                  <a href={item.link} className="inventory__item-link">
+                    Click here to buy
+                  </a>
+                </div>
+              </div>
+            </Link>
           </div>
         ))}
       </div>
